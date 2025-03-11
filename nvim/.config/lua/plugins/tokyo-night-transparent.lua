7cae3e35383e571793ac3abc06057671eccec819
Vincent <vvdspelt@gmail.com>
2025-03-11 11:20:25 +0100

    Update nvim plugins

---
+++ nvim/.config/lua/plugins/tokyo-night-transparent.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "moon",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
